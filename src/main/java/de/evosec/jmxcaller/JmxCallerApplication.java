a0f7a71ce471bb68f72477d66298cc4458eb5335
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2017-08-10 09:44:17 +0200

    Initial import

---
+++ src/main/java/de/evosec/jmxcaller/JmxCallerApplication.java
@@ -0,0 +1,12 @@
+package de.evosec.jmxcaller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmxCallerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmxCallerApplication.class, args);
+	}
+}
---
+++ src/test/java/de/evosec/jmxcaller/JmxCallerApplicationTests.java
@@ -0,0 +1,16 @@
+package de.evosec.jmxcaller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JmxCallerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
